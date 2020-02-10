7c320b1684545c3622b5216f5a7e373856fef889
Apurv-Bajaj <apurvabajaj007@gmail.com>
2020-02-11 03:16:26 +0530

    Initial files

---
+++ hash.h
@@ -0,0 +1,19 @@
+struct hashNode {
+    char* keyword;
+    struct hashNode *next;
+};
+
+struct hashEntry {
+    int size;
+    struct hashNode *first;
+};
+
+struct hashTable {
+    int size;
+    struct hashEntry *entries;
+};
+
+void addKeyword(char *keyword);
+bool searchKeyword(char *keyword);
+int hashFunction(char *keyword);
+struct hashTable* createHashTable(int size);
