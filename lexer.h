4fa4ea49f43f7a0d4ec7e275e54be4c649ab114c
Apurv-Bajaj <apurvabajaj007@gmail.com>
2020-02-12 22:18:28 +0530

    lexer proto

--- lexer.h
+++ lexer.h
@@ -1,37 +1,17 @@
-struct token {
-    int type;
-    union {
-        int num;
-        float rnum;
-        char *lexeme;
-    } value;
-};
+// struct tokenNode* pop_front();
 
-struct tokenNode {
-    struct token *tk;
-    struct tokenNode *next;
-};
+// void push_back(struct token *tk);
 
-struct tokenQueue {
-    int size;
-    struct tokenNode *first;
-    struct tokenNOde *last;
-};
+// void add_num(int n);
+// void add_rnum(float f);
+// void add_lexeme(char *lex);
 
-struct tokenNode* pop_front();
+// char* get_block(FILE *fp);
 
-void push_back(struct token *tk);
+// void DFA(char *buffer); //static line no
 
-void add_num(int n);
-void add_rnum(float f);
-void add_lexeme(char *lex);
+// void fill_keywords();
 
-char* get_block(FILE *fp);
-
-void DFA(char *buffer); //static line no
-
-void fill_keywords();
-
-typedef enum {
-
-} tokenType;
+FILE* getStream(FILE *fp);
+struct tokenInfo* getNextToken(char *buffer);
+void removeComments(char *testcaseFile, char *cleanFile);
