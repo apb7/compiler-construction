de82b4b76480a5c92d82ffa480c3589fcf02b7b4
Apurv-Bajaj <apurvabajaj007@gmail.com>
2020-03-26 20:22:01 +0530

    Init astDef.h and ast.h

---
+++ astDef.h
@@ -0,0 +1,24 @@
+// Group Number: 31
+// MADHUR PANWAR   2016B4A70933P
+// TUSSANK GUPTA   2016B3A70528P
+// SALMAAN SHAHID  2016B4A70580P
+// APURV BAJAJ     2016B3A70549P
+// HASAN NAQVI     2016B5A70452P
+
+#ifndef ASTDEF_H
+#define ASTDEF_H
+
+#include "parserDef.h"
+
+unsigned int number_of_ASTNodes;
+struct ASTNode {
+    gSymbol gs;
+    tokenInfo *tk;
+    struct ASTNode *next; // Sibling of the ASTNode.
+    struct ASTNode *child; // Child of the ASTNode.
+    struct ASTNode *parent; // Parent of the ASTNode.
+};
+
+typedef struct ASTNode ASTNode;
+
+#endif
