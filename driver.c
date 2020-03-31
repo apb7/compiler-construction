d0f9c97c19f661362458e0063d8b591cb3d32a83
Madhur Panwar <mdrpanwar@gmail.com>
2020-04-01 01:58:17 +0530

    minor changes to make clion executable code and added symbol table specs file and other files

--- driver.c
+++ driver.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[]) {
 
     fillHashTable(keywords,keyword_ht);
 
-    populateGrammarStruct("grammar.txt");
+    populateGrammarStruct("../grammar.txt");
 
 //     printGrammar();
 
---
+++ symbolTb.c
@@ -0,0 +1,4 @@
+//
+// Created by Madhur on 31-03-2020.
+//
+
