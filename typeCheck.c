6e153a13621584c7efecb93357d8834f4bcd2679
hn98 <hasannaqvi074@gmail.com>
2020-04-04 23:57:03 +0530

    typecheck rules fixes

--- typeCheck.c
+++ typeCheck.c
@@ -5,18 +5,11 @@ typedef enum{
 dataType get_expression_type(ASTNode *ptr) {
     switch(ptr->gs) {
 
-        // ALWAYS binary operators called here
+        // Arithemetic operators, only binary forms called here
         case g_PILUS:
         case g_MINUS:
         case g_MUL:
         case g_DIV:
-
-        case g_LT:
-        case g_LE:
-        case g_GT:
-        case g_GE:
-        case g_EQ:
-        case g_NE:
         {
             dataType t1 = get_expression_type(ptr->child);
             dataType t2 = get_expression_type(ptr->child->next);
@@ -30,6 +23,27 @@ dataType get_expression_type(ASTNode *ptr) {
             return T_ERROR;
         }
 
+        // Relational operators
+        case g_LT:
+        case g_LE:
+        case g_GT:
+        case g_GE:
+        case g_EQ:
+        case g_NE:
+        {
+            dataType t1 = get_expression_type(ptr->child);
+            dataType t2 = get_expression_type(ptr->child->next);
+
+            if (t1 == T_INTEGER && t2 == T_INTEGER)
+                return T_BOOLEAN;
+
+            if (t1 == T_REAL && t2 == T_REAL)
+                return T_BOOLEAN;
+            
+            return T_ERROR;
+        }
+
+        // Logical operators
         case g_AND:
         case g_OR:
         {
@@ -42,7 +56,17 @@ dataType get_expression_type(ASTNode *ptr) {
             return T_ERROR;
         }
 
+        case g_NUM:
+            return T_INTEGER;
 
+        case g_RNUM:
+            return T_REAL;
+
+        case g_TRUE:
+        case g_FALSE:
+            return T_BOOLEAN;
+
+        // TODO: get type from symbol table!
 
     }
 }
