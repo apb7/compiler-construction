3fc2bd5912a582c30672d0bf425f0426deac44f8
Madhur Panwar <mdrpanwar@gmail.com>
2020-04-05 13:24:43 +0530

    implemented some functions and refactored code

--- symbolTable.h
+++ symbolTable.h
@@ -4,11 +4,12 @@
 // SALMAAN SHAHID  2016B4A70580P
 // APURV BAJAJ     2016B3A70549P
 // HASAN NAQVI     2016B5A70452P
-
+#include "astDef.h"
+#include "symbolTableDef.h"
 #ifndef SYMBOLTABLE_H
 #define SYMBOLTABLE_H
 
 //TODO: put all the declarations here once symbolTable.c is done
-
+void boundsCheckIfStatic(ASTNode *idNode, ASTNode *idOrNumNode, symFuncInfo *funcInfo, symbolTable *currST);
 
 #endif //SYMBOLTABLE_H
