45c965a6188745638ba2b63284e1e877b302ae5a
Apurv-Bajaj <apurvabajaj007@gmail.com>
2020-04-15 13:36:42 +0530

    codeGen.c : IO Stmt

--- codeGen.c
+++ codeGen.c
@@ -20,8 +20,20 @@ void generateCode(ASTNode* root, symbolTable* symT, FILE* fp) {
         case g_program:
         {
             fprintf(fp, "section .data\n");
-            fprintf(fp,"\tinputFormat: db \"%%hd\",0\n");
-            fprintf(fp,"\toutputFormat: db \"%%hd\",10,0,\n");
+            fprintf(fp,"\tmsgBoolean: db \"Input: Enter a boolean value:\", 10, 0\n");
+            fprintf(fp,"\tinputBoolean: db \"%d\", 0\n");
+
+            fprintf(fp,"\tmsgInt: db \"Input: Enter an integer value:\", 10, 0\n");
+            fprintf(fp,"\tinputInt: db \"%d\", 0\n");
+
+            fprintf(fp,"\tmsgFloat: db \"Input: Enter a float value:\", 10, 0\n");
+            fprintf(fp,"\tinputFloat: db \"%f\",0\n");
+
+            fprintf(fp,"\toutputBoolean: db \"Output: %s\", 10, 0,\n");
+
+            fprintf(fp,"\toutputInt: db \"Output: %d\", 10, 0,\n");
+
+            fprintf(fp,"\toutputFloat: db \"Output: %f\", 10, 0,\n");
 
             fprintf(fp, "section .text\n");
             fprintf(fp, "\tglobal main\n");
@@ -90,15 +102,34 @@ void generateCode(ASTNode* root, symbolTable* symT, FILE* fp) {
             varType idVarType = siblingId->stNode->info.var.vtype;
 
             if(idVarType.vaType == VARIABLE) {
-                if(idVarType.baseType == g_BOOLEAN) {
+                // More registers need to me pushed due to preserve
+                // their values.
+                // BEWARE: Number of pushes here should be odd.
+                // push rbx
+                fprintf(fp, "\tpush rbp\n");
 
+                if(idVarType.baseType == g_BOOLEAN) {
+                    fprintf(fp, '\tlea rdi, [msgBoolean]\n');
+                    fprintf(fp, '\tcall printf\n');
+                    fprintf(fp, '\tlea rdi, [inputBoolean]\n');
+                    fprintf(fp, '\tcall scanf\n');
+                    // Scanned int goes to rax or rdx:rax.
+                    // Scanned float goes to xmm0 or xmm1:xmm0.
                 }
                 else if(idVarType.baseType == g_NUM) {
-
+                    fprintf(fp, '\tlea rdi, [msgInt]\n');
+                    fprintf(fp, '\tcall printf\n');
+                    fprintf(fp, '\tlea rdi, [inputInt]\n');
+                    fprintf(fp, '\tcall scanf\n');
                 }
                 else /* RNUM */{
-
+                    fprintf(fp, '\tlea rdi, [msgFloat]\n');
+                    fprintf(fp, '\tcall printf\n');
+                    fprintf(fp, '\tlea rdi, [inputFloat]\n');
+                    fprintf(fp, '\tcall scanf\n');
                 }
+
+                fprintf(fp, "\tpop rbp\n");
             }
             else /* Arrays */ {
 
